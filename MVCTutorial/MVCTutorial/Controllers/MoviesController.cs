d790ef8e9fff4a1d14cb8c5d5ca0f3dd3171627c
EGM88 <emanuelmorais@outlook.pt>
2017-01-03 23:16:56 +0000

    Movie added. Theme added. Title changed

---
+++ MVCTutorial/MVCTutorial/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTutorial.Models;
+
+namespace MVCTutorial.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Cenas Maradas",
+                Id = Guid.NewGuid()
+            };
+            return View(movie);
+        }
+    }
+}
---
+++ MVCTutorial/MVCTutorial/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTutorial.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
